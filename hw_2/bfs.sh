6677cac9c980d6e87fe86e8cbe584c2633acc8df
Steve <stevegardiner26@aol.com>
2020-06-03 21:09:27 -0400

    Added DFS Algorithim

--- hw_2/bfs.sh
+++ hw_2/bfs.sh
@@ -19,5 +19,19 @@ then
 		done
 	done
 else
+	remember=($dir)
 	echo "Depth"
+	for ((i=0;i<depth;i++)); do
+		for b in ${remember[@]}; do
+			unset #{remember[$b]}
+			for ((j=0;j<breadth;j++)); do
+				if [[ "$i" = $((depth - 1)) ]]; then
+					mkdir -p $b/$j
+				fi
+				if [[ ! " ${remember[@]} " =~ "$b/$j" ]]; then
+					remember=("$b/$j" "${remember[@]}")
+				fi
+			done
+		done
+	done
 fi
