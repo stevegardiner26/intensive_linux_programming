4caf9e97851576abd6883bcfabf74e837cddac81
Steve <stevegardiner26@aol.com>
2020-07-28 20:50:41 -0400

    Finished Python Script to Fetch Data

--- web_processing/fetch_stock_data.sh
+++ web_processing/fetch_stock_data.sh
@@ -10,7 +10,7 @@ current_date=`date +"%Y_%m_%d_%H_%M_%S"`
 html_file_name="yahoo_${current_date}.html"
 # wget
 # java -jar /usr/share/java/tagsoup-1.2.1.jar --files $html_file_name
-# Calculate the final file xfn=${hfn/.html/.xhtml}
+# converted_file_name=${html_file_name/.html/.xhtml}
 # run python code to extract and send to db / python database.py $html_file_name
 # sleep 60 // Sleep for 1min
 # timer + 1
