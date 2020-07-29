e43318b3fb68cf951c0427ef6b3915a573ecec0d
Steve <stevegardiner26@aol.com>
2020-07-28 20:57:13 -0400

    Finished Bash Script to Fetch Stock Data

--- web_processing/fetch_stock_data.sh
+++ web_processing/fetch_stock_data.sh
@@ -2,15 +2,15 @@
 
 index_page="https://finance.yahoo.com/most-active"
 
-wget -O $html_file_name $index_page
-
-# let timer var = 0
-# while loops to make sure this executes 60 times so it gets every minute in an hour while timervar < 60
-current_date=`date +"%Y_%m_%d_%H_%M_%S"`
-html_file_name="yahoo_${current_date}.html"
-# wget
-# java -jar /usr/share/java/tagsoup-1.2.1.jar --files $html_file_name
-# converted_file_name=${html_file_name/.html/.xhtml}
-# run python code to extract and send to db / python database.py $html_file_name
-# sleep 60 // Sleep for 1min
-# timer + 1
+timer=0
+while [ $timer -lt 60 ]
+do
+    current_date=`date +"%Y_%m_%d_%H_%M_%S"`
+    html_file_name="yahoo_${current_date}.html"
+    wget -O $html_file_name $index_page
+    java -jar /usr/share/java/tagsoup-1.2.1.jar --files $html_file_name
+    converted_file_name=${html_file_name/.html/.xhtml}
+    python3 database.py $converted_file_name
+    sleep 60
+    timer=$[$timer+1]
+done 
